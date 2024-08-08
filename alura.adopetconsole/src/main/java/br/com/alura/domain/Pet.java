7389fbdd8f65377a8960db715b9f9776296b800d
andre abreu <aanddre@gmail.com>
2024-08-08 12:50:09 -0300

    Java Refatoração - 03.02-Definindo domínios

---
+++ alura.adopetconsole/src/main/java/br/com/alura/domain/Pet.java
@@ -0,0 +1,21 @@
+package br.com.alura.domain;
+
+public class Pet {
+
+	public Pet(String tipo, String nome, String raca, String idade, String cor, String peso) {
+		this.tipo = tipo;
+		this.nome = nome;
+		this.raca = raca;
+		this.idade = idade;
+		this.cor = cor;
+		this.peso = peso;
+	}
+
+	private String tipo;
+	private String nome;
+	private String raca;
+	private String idade;
+	private String cor;
+	private String peso;
+
+}
