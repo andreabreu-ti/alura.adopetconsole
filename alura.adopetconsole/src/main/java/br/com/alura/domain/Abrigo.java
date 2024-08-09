dc162da2f7febab1f5599c0eebc76d6acdc8ca4c
andre abreu <aanddre@gmail.com>
2024-08-09 09:24:56 -0300

    Java Refatoração - 03.05-Conhecendo o jackson-databind

--- alura.adopetconsole/src/main/java/br/com/alura/domain/Abrigo.java
+++ alura.adopetconsole/src/main/java/br/com/alura/domain/Abrigo.java
@@ -1,15 +1,24 @@
 package br.com.alura.domain;
 
 public class Abrigo {
-	
+
 	public Abrigo(String nome, String telefone, String email) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.email = email;
 	}
 
+	private Long id;
 	private String nome;
 	private String telefone;
 	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
 
 }
